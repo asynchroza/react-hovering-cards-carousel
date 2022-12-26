5bee2dd5c94ebc7014849e35b4d3b9319b9043e8
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-12-26 19:49:40 +0200

    Add translate logic for EventCarousel

---
+++ src/libs/EventCarousel/EventCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Event } from "../ObjectDefinitions";
+
+type Props = {
+  event: Event;
+};
+
+export const EventCard = ({ event }: Props) => {
+  return (
+    <div className="event-card-container">
+      <img src={event.image} className="event-card-images"/>
+    </div>
+  );
+};
--- src/libs/EventCarousel/EventCarousel.tsx
+++ src/libs/EventCarousel/EventCarousel.tsx
@@ -1,10 +1,31 @@
-import React, { ComponentProps, PropsWithChildren } from "react";
+import React from "react";
 import { Event } from "../ObjectDefinitions";
+import "./event_carousel.css";
+import { EventCard } from "./EventCard";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { useState } from "react";
 
 type Props = {
   events: Event[];
 };
 
 export const EventCarousel = ({ events }: Props) => {
-  return <h1>WORKING</h1>;
+  const [position, setPosition] = useState(0);
+
+  return (
+    <div className="event-carousel-container">
+      <FaArrowCircleLeft className="arrow" />
+      <div className="event-carousel-map">
+        <div
+          className="event-carousel-map moving"
+          style={{ transform: `translateX(${position}px)` }}
+        >
+          {events.map((event, index) => (
+            <EventCard event={event} key={index} />
+          ))}
+        </div>
+      </div>
+      <FaArrowCircleLeft className="arrow right" />
+    </div>
+  );
 };
